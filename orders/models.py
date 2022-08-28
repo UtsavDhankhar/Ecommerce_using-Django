32f2adb6735b9e935e915ac7577dd3db67efcde6
Utsav Dhankhar <utsav900@gmail.com>
2022-08-28 06:20:27 +0530

    complete order Payment facility And ready for deployment

--- orders/models.py
+++ orders/models.py
@@ -38,6 +38,10 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updaed_at = models.DateTimeField(auto_now = True)
 
+
+    def name(self):
+        return address
+
     def __str__(self):
         return self.address.first_name + self.address.last_name
 
@@ -45,11 +49,9 @@ class Order(models.Model):
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order , on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment , on_delete=models.SET_NULL , blank = True , null = True)
-    usesr = models.ForeignKey(Account , on_delete=models.CASCADE)
+    user = models.ForeignKey(Account , on_delete=models.CASCADE)
     product = models.ForeignKey(Product , on_delete=models.CASCADE)
-    variation = models.ForeignKey(Variation , on_delete = models.CASCADE)
-    color = models.CharField(max_length=50)
-    size = models.CharField(max_length = 50)
+    variations = models.ManyToManyField(Variation, blank=True)
     quantity = models.IntegerField()
     product_price = models.FloatField()
     ordered = models.BooleanField(default = False)
